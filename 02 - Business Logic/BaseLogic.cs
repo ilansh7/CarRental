4e0c2a14b082f80d0bd693166e68a14655189c89
Ilan <44946919+ilansh7@users.noreply.github.com>
2019-01-30 11:18:31 +0200

    Add files via upload

---
+++ 02 - Business Logic/BaseLogic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SunshineRentals
+{
+    public class BaseLogic : IDisposable
+    {
+        protected readonly CarRentalEntities DB = new CarRentalEntities();
+
+        public void Dispose()
+        {
+            DB.Dispose();
+        }
+    }
+}
